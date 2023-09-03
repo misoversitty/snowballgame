1a4a11bee3d4191d74b99127b3c42a421316c02c
misoversitty <smirennikov.i@mail.ru>
2023-09-03 17:58:08 +0300

    sides now store in set, not dict canceled several-side collisions

--- Project/Services/Collision.py
+++ Project/Services/Collision.py
@@ -1,36 +1,26 @@
 import pygame
 from Project.Entities.Player import Player
 
-delta = [2, 5]
+delta = [7, 5]
 
 class Collision:
     def __init__(self, mainSprite: pygame.sprite, obstacle: pygame.sprite):
-        self.mainSprite = mainSprite
+        self.mainObject = mainSprite
         self.obstacle = obstacle
-        self.blockedSidesOfMainSprite = self.guessSeveralSides()
+        self.blockedSides = self.guessSeveralSides()
 
     def guessSeveralSides(self):
-        side = {
-            "UP": False,
-            "DOWN": False,
-            "LEFT": False,
-            "RIGHT": False
-        }
-        print(f"    self: top={self.mainSprite.rect.top}, bottom={self.mainSprite.rect.bottom}")
-        print(f"obstacle: top={self.obstacle.rect.top}, bottom={self.obstacle.rect.bottom}")
-        croppedArea = self.mainSprite.rect.clip(self.obstacle.rect)
-        if croppedArea.width >= delta[0]:
-            pass
-        if self.mainSprite.rect.top < self.obstacle.rect.bottom and self.mainSprite.rect.centery > self.obstacle.rect.centery:
-            side["UP"] = True
-        elif self.mainSprite.rect.bottom > self.obstacle.rect.top and self.mainSprite.rect.centery < self.obstacle.rect.centery:
-            side["DOWN"] = True
-        if self.mainSprite.rect.left < self.obstacle.rect.right and self.mainSprite.rect.centerx > self.obstacle.rect.centerx:
-            side["LEFT"] = True
-        elif self.mainSprite.rect.right > self.obstacle.rect.left and self.mainSprite.rect.centerx < self.obstacle.rect.centerx:
-            side["RIGHT"] = True
-        return side
+        side = set()
+        croppedArea = self.mainObject.rect.clip(self.obstacle.rect)
+        if delta[0] <= croppedArea.width < croppedArea.height:
+            if self.mainObject.rect.centerx > croppedArea.centerx:
+                side.add("LEFT")
+            elif self.mainObject.rect.centerx < croppedArea.centerx:
+                side.add("RIGHT")
+        elif delta[0] <= croppedArea.height < croppedArea.width:
+            if self.mainObject.rect.centery > croppedArea.centery:
+                side.add("UP")
+            elif self.mainObject.rect.centery < croppedArea.centery:
+                side.add("DOWN")
 
-    def getBlockedSides(self):
-        res = list(map(lambda item: item[0] if item[1] else None, self.blockedSidesOfMainSprite.items()))
-        return res
+        return side
