3cae70303592740debc84fff4eb49365c2a0be85
misoversit <smirennikov.i@mail.ru>
2023-09-20 21:15:13 +0300

    +-

--- Project/Services/ImageContainer.py
+++ Project/Services/ImageContainer.py
@@ -13,13 +13,13 @@ class ImageContainer:
                      'down-right': []}
         for i in imgs:
             self.dict['up'].append(i)
-            self.dict['down'].append(ImageLoader.RotateImage(i, 180))
-            self.dict['left'].append(ImageLoader.RotateImage(i, 90))
-            self.dict['right'].append(ImageLoader.RotateImage(i, 270))
-            self.dict['up-left'].append(ImageLoader.RotateImage(i, 45))
-            self.dict['up-right'].append(ImageLoader.RotateImage(i, 325))
-            self.dict['down-left'].append(ImageLoader.RotateImage(i, 135))
-            self.dict['down-right'].append(ImageLoader.RotateImage(i, 225))
+            self.dict['down'].append(ImageLoader.rotateImage(i, 180))
+            self.dict['left'].append(ImageLoader.rotateImage(i, 90))
+            self.dict['right'].append(ImageLoader.rotateImage(i, 270))
+            self.dict['up-left'].append(ImageLoader.rotateImage(i, 45))
+            self.dict['up-right'].append(ImageLoader.rotateImage(i, 325))
+            self.dict['down-left'].append(ImageLoader.rotateImage(i, 135))
+            self.dict['down-right'].append(ImageLoader.rotateImage(i, 225))
 
     def __getitem__(self, item):
         return self.dict[item]
