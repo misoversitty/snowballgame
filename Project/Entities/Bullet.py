1fc329a564baefdd553bc49cabb731df15f047b6
misoversit <smirennikov.i@mail.ru>
2023-09-20 21:15:35 +0300

    +-

--- Project/Entities/Bullet.py
+++ Project/Entities/Bullet.py
@@ -5,7 +5,7 @@ from Project.Services.ImageLoader import ImageLoader
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, *groups, **kwargs):
         super().__init__(*groups)
-        self.image = ImageLoader.LoadImage('bullet.png')
+        self.image = ImageLoader.loadImage('bullet.png')
         self.rect = self.image.get_rect()
         self.rect.center = (400, 300)
 
