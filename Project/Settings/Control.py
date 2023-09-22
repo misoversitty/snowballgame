4dc16bd5eb1c5968ff2483546b295eda46167d3d
misoversit <smirennikov.i@mail.ru>
2023-09-22 21:30:11 +0300

    Reworked to load settings from settings-file

--- Project/Settings/Control.py
+++ Project/Settings/Control.py
@@ -1,8 +1,6 @@
 class Control:
-    def __init__(self, k_up: str, k_down: str, k_left: str, k_right: str):
-        self.assignment = {
-            k_up: "Up",
-            k_down: "Down",
-            k_left: "Left",
-            k_right: "Right"
-        }
+    def __init__(self, *args, **kwargs):
+        self.assignment = {}
+
+    def set(self, key: str, action: str):
+        self.assignment[key.lower()] = action
