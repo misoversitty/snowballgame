1105c52ce5b8298f2d79367a822f06832208378d
misoversit <smirennikov.i@mail.ru>
2023-09-22 13:33:34 +0300

    self.dict now contains numpy arrays

--- Project/DataStructures/ImageContainer.py
+++ Project/DataStructures/ImageContainer.py
@@ -1,25 +1,30 @@
 from Project.Services.ImageLoader import ImageLoader
+import numpy as np
+from pygame import Surface
 
 class ImageContainer:
-    def __init__(self, *imgs):
+    def __init__(self, *images):
         super().__init__()
-        self.dict = {'up': [],
-                     'down': [],
-                     'left': [],
-                     'right': [],
-                     'up-left': [],
-                     'up-right': [],
-                     'down-left': [],
-                     'down-right': []}
-        for i in imgs:
-            self.dict['up'].append(i)
-            self.dict['down'].append(ImageLoader.rotateImage(i, 180))
-            self.dict['left'].append(ImageLoader.rotateImage(i, 90))
-            self.dict['right'].append(ImageLoader.rotateImage(i, 270))
-            self.dict['up-left'].append(ImageLoader.rotateImage(i, 45))
-            self.dict['up-right'].append(ImageLoader.rotateImage(i, 325))
-            self.dict['down-left'].append(ImageLoader.rotateImage(i, 135))
-            self.dict['down-right'].append(ImageLoader.rotateImage(i, 225))
+        length = len(images)
+        self.dict = {
+            'up': np.empty(shape=[length], dtype=Surface),
+            'down': np.empty(shape=[length], dtype=Surface),
+            'left': np.empty(shape=[length], dtype=Surface),
+            'right': np.empty(shape=[length], dtype=Surface),
+            'up-left': np.empty(shape=[length], dtype=Surface),
+            'up-right': np.empty(shape=[length], dtype=Surface),
+            'down-left': np.empty(shape=[length], dtype=Surface),
+            'down-right': np.empty(shape=[length], dtype=Surface)
+        }
+        for count, image in enumerate(images):
+            self.dict['up'][count] = image
+            self.dict['down'][count] = ImageLoader.rotateImage(image, 180)
+            self.dict['left'][count] = ImageLoader.rotateImage(image, 90)
+            self.dict['right'][count] = ImageLoader.rotateImage(image, 270)
+            self.dict['up-left'][count] = ImageLoader.rotateImage(image, 45)
+            self.dict['up-right'][count] = ImageLoader.rotateImage(image, 325)
+            self.dict['down-left'][count] = ImageLoader.rotateImage(image, 135)
+            self.dict['down-right'][count] = ImageLoader.rotateImage(image, 225)
 
     def __getitem__(self, item):
         return self.dict[item]
