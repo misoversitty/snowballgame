b00d176ca6124810435a8e8f4fa08a5b70a28723
misoversit <smirennikov.i@mail.ru>
2023-09-24 21:20:52 +0300

    +

---
+++ Project/DataStructures/Acceleration.py
@@ -0,0 +1,4 @@
+class Acceleration:
+    def __init__(self, starting: int, slowing: int):
+        self.x = {"STARTING": starting, "SLOWING": slowing}
+        self.y = {"STARTING": starting, "SLOWING": slowing}
