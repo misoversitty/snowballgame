a1c9c244ffc0e1063575fb6a187f71aadb93c498
misoversit <smirennikov.i@mail.ru>
2023-09-26 18:28:05 +0300

    Removed

--- Project/DataStructures/Acceleration.py
+++
@@ -1,4 +0,0 @@
-class Acceleration:
-    def __init__(self, starting: int, slowing: int):
-        self.x = {"STARTING": starting, "SLOWING": slowing}
-        self.y = {"STARTING": starting, "SLOWING": slowing}
