febb2f21f6aa3f5db43ff98c606e1eb347f2b807
misoversit <smirennikov.i@mail.ru>
2023-09-26 18:29:10 +0300

    Added copy-method

--- Project/DataStructures/Facing.py
+++ Project/DataStructures/Facing.py
@@ -1,4 +1,13 @@
+from copy import copy
+
+
 class Facing:
     def __init__(self, x=0, y=-1):
         self.x = x
         self.y = y
+
+    def __call__(self):
+        return self.x, self.y
+
+    def copy(self):
+        return copy(self)
