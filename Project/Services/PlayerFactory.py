f4b7c28a15751de96dd90a4607fa206866877073
misoversit <smirennikov.i@mail.ru>
2024-02-01 04:19:04 +0300

    Rename 'playerInitializer' => 'playerFactory'

--- Project/Services/PlayerFactory.py
+++ Project/Services/PlayerFactory.py
@@ -1,8 +1,9 @@
 import numpy as np
+
 from Project.Entities.Player import Player
 
 
-class PlayerInitializer:
+class PlayerFactory:
     def __new__(cls, count):
         players = np.array([Player(number=i) for i in np.arange(count)], dtype=Player)
         return players
